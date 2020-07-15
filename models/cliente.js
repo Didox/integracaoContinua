53bf6afdca2424e0bb7a019ccb6561e58288f21f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-07-15 17:19:46 -0300

    versionando com erro

--- models/cliente.js
+++ models/cliente.js
@@ -5,4 +5,27 @@ module.exports = class Cliente {
         this.email = email;
         this.dataCadastro = new Date();
     }
+
+    static validarCPF(strCPF){
+        return true
+        strCPF = strCPF.replace(/\.|\-/g, "")
+        var soma;
+            var resto;
+            soma = 0;
+        if (strCPF == "00000000000") return false;
+            
+        for (let i=1; i<=9; i++) soma = soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        resto = (soma * 10) % 11;
+        
+            if ((resto == 10) || (resto == 11))  resto = 0;
+            if (resto != parseInt(strCPF.substring(9, 10)) ) return false;
+        
+        soma = 0;
+            for (let i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+            resto = (soma * 10) % 11;
+        
+            if ((resto == 10) || (resto == 11))  resto = 0;
+            if (resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+            return true;
+    }
 }
--- spec/cliente.spec.js
+++ spec/cliente.spec.js
@@ -3,7 +3,14 @@ const Cliente = require("../models/cliente")
 describe('Clientes', () => {
     test('Deve fazer o teste de match', () => {
         const cliente = new Cliente();
-
         expect(cliente.nome).not.beUndefined;
+    });
+
+    test('Deve testar um CPF válido', () => {
+        expect(Cliente.validarCPF("134.841.820-67")).toBe(true);
+    });
+
+    test('Deve testar um CPF inválido', () => {
+        expect(Cliente.validarCPF("134.841.820-17")).toBe(false);
     });
 })
